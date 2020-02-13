c6125219f91488a5d28d35ec3bc9e2bdb7104b1b
mosiahi <mosiahi0201@gmail.com>
2020-02-13 14:36:16 +0100

    menu

---
+++ HorrorGame/Assets/Goback.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goback : MonoBehaviour
+{
+    static public bool myGoBackToMain = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Intropart"))
+        {
+            myGoBackToMain = false;
+        }
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Menu"))
+        {
+            myGoBackToMain = true;
+        }
+    }
+}
